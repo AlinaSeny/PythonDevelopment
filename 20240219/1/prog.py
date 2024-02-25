dba8c9860121b210a3433eff8f9e6aa1542ddcf9
AlinaSeny <meursault307@gmail.com>
2024-02-25 19:43:47 +0300

    complete 2nd step

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 if len(sys.argv) == 1:
@@ -11,3 +12,16 @@ else:
 		branches = os.listdir(path + '/.git/refs/heads')
 		for branch in branches:
 			print(branch)
+	elif len(sys.argv) == 3:
+		branch = sys.argv[2]
+		if branch not in os.listdir(path + '/.git/refs/heads'):
+			print(f"Branch {branch} doesn't exist")
+			sys.exit(1)
+		else:
+			f = open(path + '/.git/refs/heads/' + branch, 'r')
+			last_com = f.readline().strip()
+			f.close()
+			f = open(path + '/.git/objects/' + last_com[:2] + '/' + last_com[2:], 'rb')
+			header, _, body = zlib.decompress(f.read()).partition(b'\00')
+			print(body.decode())
+			f.close()
