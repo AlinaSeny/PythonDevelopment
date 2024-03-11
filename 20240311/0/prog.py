e3908b1e046eb93ec459cb558f0abbd07423b0a2
AlinaSeny <meursault307@gmail.com>
2024-03-11 13:44:40 +0300

    task done

--- 20240311/0/prog.py
+++ 20240311/0/prog.py
@@ -1,1 +1,16 @@
-print('Hello, git!')
+import cmd
+
+class Echoer(cmd.Cmd):
+	'''Dumb echo command REPL'''
+	propmpt = ':->'
+
+	def do_echo(self, args):
+		print(args)
+
+	def do_EOF(self, args):
+		return True
+
+
+if __name__ == '__main__':
+	Echoer().cmdloop()
+
